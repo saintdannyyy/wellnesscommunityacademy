6556449ac5912906026fc9f2d43edbaba9ecbd8f
Saintdannyyy <danieltesla746@gmail.com>
2024-12-02 14:06:52 +0000

    Updated db conns

--- admin/db/login_conn.php
+++ admin/db/login_conn.php
@@ -1,6 +1,9 @@
 <?php
-    // Connect to database
-    $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
+    // Connect to prod database
+    // $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
+
+    // Connect to the dev database
+    $mysqli = new mysqli("localhost", "root", "", "wellness");
 
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
--- conn/conn.php
+++ conn/conn.php
@@ -1,6 +1,10 @@
 <?php
-    $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
-    // $mysqli = new mysqli("localhost", "root", "", "wellness1_ebooks");
+    // Connect to prod database
+    // $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
+
+    // Connect to the dev database
+    $mysqli = new mysqli("localhost", "root", "", "wellness");
+
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
