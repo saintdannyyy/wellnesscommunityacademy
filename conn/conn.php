279e351ce6747ce1b86d7ea4d0b7dd8facb03a85
Saintdannyyy <danieltesla746@gmail.com>
2024-12-03 13:44:29 +0000

    Add environment-based database connection logic and session start

--- conn/conn.php
+++ conn/conn.php
@@ -1,11 +1,12 @@
 <?php
-    // Connect to prod database
-    // $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
-
-    // Connect to the dev database
-    $mysqli = new mysqli("localhost", "root", "", "wellness");
-
+require_once __DIR__ . '../../config/loadENV.php';
+    if ($_ENV['APP_ENV'] === 'dev') {
+        $mysqli = new mysqli("localhost", "root", "", "wellness");
+    } else {
+        $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
+    }
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
+    session_start();
 ?>
