d2ea68b9350ea000c1c08948d3a0293d5c93c493
Saintdannyyy <danieltesla746@gmail.com>
2024-12-10 10:50:03 +0000

    Refactor database connection logic to use environment variables for different environments

--- admin/db/connection.php
+++ admin/db/connection.php
@@ -9,12 +9,15 @@
         exit();
     }
 
-    // Connect to prod database
-    // $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
-
-    // Connect to the dev database
-    $mysqli = new mysqli("localhost", "root", "", "wellness");
-    
+    require_once __DIR__ . '../../../config/loadENV.php';
+    if ($_ENV['APP_ENV'] === 'dev') {
+        $mysqli = new mysqli("localhost", "root", "", "wellness");
+    } else {
+        $mysqli = new mysqli("localhost", "wellnes1_wellnes1", "@1ph@5t0rmerh05t", "wellnes1_ebooks");
+    }
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }    
     // Check for database connection error
     if ($mysqli->connect_error) {
         // Log the error and show a generic error message
